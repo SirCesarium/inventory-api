d2f0b9cbdc8511f8ecc44928656e8ba2061e7286
SirCesarium <239650612+SirCesarium@users.noreply.github.com>
2026-05-28 17:23:51 -0400

    fix(model): add HasApiTokens from sanctum

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,7 @@ use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 #[ObservedBy(AuditObserver::class)]
 #[Fillable(['name', 'email', 'password'])]
@@ -19,7 +20,7 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * Get the attributes that should be cast.
