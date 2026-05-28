e8c9883bbb44a4c8f22e09fcb4f10867a6a69dda
SirCesarium <239650612+SirCesarium@users.noreply.github.com>
2026-05-28 18:40:02 -0400

    refactor: optimize auth login/logout queries

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 
@@ -15,18 +14,20 @@ class AuthController extends Controller
      */
     public function login(Request $request)
     {
-        $credentials = $request->validate([
+        $fields = $request->validate([
             'email' => 'required|email',
             'password' => 'required|string',
         ]);
 
-        if (! Auth::guard('web')->attempt($credentials)) {
+        $user = User::query()->where('email', $fields['email'])->first();
+
+        if (! $user || ! Hash::check($fields['password'], $user->password)) {
             throw ValidationException::withMessages([
                 'email' => ['Incorrect credentials.'],
             ]);
         }
 
-        $token = $request->user()->createToken('auth_token')->plainTextToken;
+        $token = $user->createToken('auth_token')->plainTextToken;
 
         return response()->json([
             'message' => 'Login success',
@@ -77,7 +78,7 @@ class AuthController extends Controller
      */
     public function logout(Request $request)
     {
-        $request->user()->tokens()->delete();
+        $request->user()->currentAccessToken()->delete();
 
         return response()->json([
             'message' => 'Logout.',
