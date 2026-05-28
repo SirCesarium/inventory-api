5939d77a99d4c977210fea98adb718ffa5738acc
SirCesarium <239650612+SirCesarium@users.noreply.github.com>
2026-05-28 12:59:53 -0400

    feat(models): create role model

---
+++ app/Models/Role.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    protected $fillable = ['name'];
+
+    /**
+     * Get role users
+     */
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    /**
+     * Get role permissions
+     */
+    public function permissions()
+    {
+        return $this->belongsToMany(Permission::class);
+    }
+}
